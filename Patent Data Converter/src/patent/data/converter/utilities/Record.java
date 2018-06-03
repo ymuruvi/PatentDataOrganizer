8178aac2d7f066167d66631c7f41505ce4b1699f
Nano <ymuruvi@gmail.com>
2018-06-03 16:09:58 -0400

    Searching for individual nodes works well.

--- Patent Data Converter/src/patent/data/converter/utilities/Record.java
+++ Patent Data Converter/src/patent/data/converter/utilities/Record.java
@@ -23,6 +23,8 @@ public class Record {
     enum DocumentType {
         PATENT, PATENT_APP
     };
+    
+    private HashMap dataNodes;
 
     private String docNum;
     private String appNum;
@@ -33,6 +35,11 @@ public class Record {
     private Inventor[] inventors;
 
     private boolean searchingForNode;
+    
+    
+    public Record(){
+        dataNodes = new HashMap();
+    }
 
     /**
      * Parses the .xml file at the given location looking for key data points
@@ -62,13 +69,28 @@ public class Record {
                         + Tools.Contstants.ANSI_RESET);
                 
                 listOfFields = xmlDoc.getElementsByTagName("ca-bibliographic-data");
+                
+                /*
                 nl = getElementAndAttribute(listOfFields, "invention-title", "lang");
                 documentID = getElementNodeList(listOfFields, "publication-reference");
+                */
                 
                 n = null;
-                getNode(n, listOfFields, "ca-administrative-status-change");
+                
+                SearchNode[] searchNodes = { new SearchNode("publication-reference")
+                        , new SearchNode("classifications-ipcr"), new SearchNode("classification-national")
+                        , new SearchNode("application-reference"), new SearchNode("language-of-filing")
+                        , new SearchNode("priority-claims"), new SearchNode("dates-of-public-availability")
+                        , new SearchNode("pct-or-regional-filing-data"), new SearchNode("pct-or-regional-publishing-data")
+                        , new SearchNode("ca-office-specific-bib-data"), new SearchNode("invention-title",0)
+                        , new SearchNode("invention-title",1)};
+                
+                for(SearchNode sn : searchNodes){
+                    sn.setNode(searchForNode(listOfFields, sn.getSearchTerm(),sn.getOccurance()));
+                    System.out.println(Tools.Contstants.ANSI_YELLOW+"Name: " + sn.getNodeName() + Tools.Contstants.ANSI_RESET);
+                }
+                
                 if (n != null) {
-                    System.out.println(n.getNodeName());
                 }
             } else {
                 System.out.println(Tools.Contstants.ANSI_RED + "Error reading file: "
@@ -80,15 +102,15 @@ public class Record {
     }
 
     private static Document getDocument(String docString) {
+        DocumentBuilderFactory factory;
+        DocumentBuilder builder;
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
+            factory = DocumentBuilderFactory.newInstance();
             factory.setIgnoringComments(true);
             factory.setIgnoringElementContentWhitespace(true);
             factory.setValidating(true);
-
-            DocumentBuilder builder = factory.newDocumentBuilder();
-
+            
+            builder = factory.newDocumentBuilder();
             return builder.parse(new InputSource(docString));
 
         } catch (Exception e) {
@@ -96,52 +118,68 @@ public class Record {
         }
         return null;
     }
-
+    
     /**
      * Returns the first occurrence of a node with a given name.
      *
      * @param listOfFields The list of nodes for which a node will be searched
      * for.
-     * @param name The name for which a node with a matching name will be
+     * @param searchTerm The name for which a node with a matching name will be
      * searched for.
      * @return The node with the name matching the <code>name</code> given, or
      * null if no such node is found.
      */
-    private Node getNode(Node outNode, NodeList listOfFields, String name) {
-        Node n = null;
+    private Node searchForNode(NodeList listOfFields, String searchTerm) {
+        return searchForNode(listOfFields, searchTerm, 0);
+    }
+    
+    /**
+     * 
+     * @param listOfFields
+     * @param searchTerm
+     * @param occuranceIndex
+     * @return 
+     */
+    private Node searchForNode(NodeList listOfFields, String searchTerm, int occuranceIndex) {
+        
         Node node;
+        Node out = null;
         Element element;
         String nodeName;
         int numChildNodes;
 
-        outNode = null;
         searchingForNode = true;
         try {
+            //System.out.println("Trying search for: " + name);
             for (int i = 0; i < listOfFields.getLength() && searchingForNode; i++) {
+                //System.out.println("Trying Deeper: " + i);
                 node = listOfFields.item(i);
                 nodeName = node.getNodeName();
                 numChildNodes = node.getChildNodes().getLength();
-                if (node.getNodeName().equals(name)) {
-                    System.out.println(Tools.Contstants.ANSI_CYAN
-                            + "Found: " + name + Tools.Contstants.ANSI_RESET);
-                    if (searchingForNode) {
-                        outNode = node;
-                        searchingForNode = false;
+                if (node.getNodeName().equals(searchTerm)) {
+                    if(occuranceIndex == 0){
+                        System.out.println(Tools.Contstants.ANSI_CYAN + "Found: " + searchTerm + Tools.Contstants.ANSI_RESET);
+                        if (searchingForNode) {
+                            searchingForNode = false;
+                            return node;
+                        }
+                    }else{
+                        occuranceIndex--;
                     }
                 } else {
-                    System.out.println(Tools.Contstants.ANSI_PURPLE + "Search Unsuccessful (" + node.getNodeName() + " " + searchingForNode + ")");
+                    //System.out.println(Tools.Contstants.ANSI_PURPLE + "Search Unsuccessful (" + node.getNodeName() + " " + searchingForNode + ")");
                     if (numChildNodes > 0 && searchingForNode) {
+                        //System.out.println("Going In Fam");
                         element = (Element) node;
                         NodeList list = element.getChildNodes();
-                        getNode(outNode, list, name);
+                        out = searchForNode(list, searchTerm, occuranceIndex);
                     }
                 }
             }
         } catch (Exception e) {
-            System.out.println(Tools.Contstants.ANSI_RED + "Error: "
-                    + e.getMessage() + Tools.Contstants.ANSI_RESET);
+            System.out.println(Tools.Contstants.ANSI_RED + "Error: " + e.getMessage() + Tools.Contstants.ANSI_RESET);
         }
-        return n;
+        return out;
     }
 
     /**
@@ -152,13 +190,15 @@ public class Record {
      * @return
      */
     private NodeList getElementAndAttribute(NodeList listOfFields, String elementName, String searchAttr) {
+        
         NodeList out = null;
-
         Node node;
         Element elem;
-        NodeList list;
         Element nElem;
+        NodeList list;
         NodeList eList;
+        
+        
 
         try {
             for (int i = 0; i < listOfFields.getLength(); i++) {
@@ -222,5 +262,58 @@ public class Record {
         String attr = null;
         attr = node.getNodeValue();
         return attr;
+    }
+    
+    private class SearchNode{
+        
+        private int occurance;
+        private Node node;
+        private String searchTerm;
+        
+        public SearchNode(String searchTerm, int occurance){
+            this.searchTerm = searchTerm;
+            this.occurance = occurance;
+        }
+        
+        public SearchNode (String searchTerm){
+            this(searchTerm,0);
+        }
+        
+        public SearchNode(){
+            this("",0);
+        }
+
+        public int getOccurance() {
+            return occurance;
+        }
+
+        public void setOccurance(int occurance) {
+            this.occurance = occurance;
+        }
+
+        public Node getNode() {
+            return node;
+        }
+        
+        public String getNodeName(){
+            if(node == null){
+                return "";
+            }else{
+                return node.getNodeName();
+            }
+        }
+
+        public void setNode(Node node) {
+            this.node = node;
+        }
+
+        public String getSearchTerm() {
+            return searchTerm;
+        }
+
+        public void setSearchTerm(String searchTerm) {
+            this.searchTerm = searchTerm;
+        }
+        
     }
 }
