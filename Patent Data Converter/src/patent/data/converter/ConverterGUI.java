864cbe6e9952c117ad39255d38e54d96a2270145
Nano <ymuruvi@gmail.com>
2018-06-08 17:56:39 -0400

    Slight modifications made

--- Patent Data Converter/src/patent/data/converter/ConverterGUI.java
+++ Patent Data Converter/src/patent/data/converter/ConverterGUI.java
@@ -171,7 +171,7 @@ public class ConverterGUI extends JFrame {
                              * threads has been met.
                              */
                             while (threads >= Math.min(MAX_THREADS,files.length)-1) {
-                                System.out.println("Waiting");
+                                //System.out.println("Waiting");
                             }
                             Thread handleFileThread = new Thread(new FileHandler(f));
                             threads++;
@@ -883,7 +883,7 @@ public class ConverterGUI extends JFrame {
             tab2PanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(tab2PanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 391, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -937,8 +937,8 @@ public class ConverterGUI extends JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(tabbedPane, javax.swing.GroupLayout.PREFERRED_SIZE, 441, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(tabbedPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(13, Short.MAX_VALUE))
         );
 
         pack();
